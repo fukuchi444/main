1f28f3db10cbff923fba28b8ca5deb667aff9264
Fukuchi_Miku <rikopin390@yahoo.co.jp>
2023-04-27 01:38:26 +0900

    1.3 Java導入

---
+++ HelloWorld/src/sample/selfIntroduction.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class selfIntroduction {
+
+	public static void main(String[] args) { {
+		String name = "ネ一口田土也羊大糸エ";
+		int age = 10603;
+		String hobby = "ない";
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("生まれてから" + age + "日以上経過しました。");
+		System.out.println("趣味は" + hobby + "です。");
+		}
+	}
+}
