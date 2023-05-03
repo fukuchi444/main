e67f28e95c04641c8a8f31cd9d6f22dd7aa5bb20
Fukuchi_Miku <rikopin390@yahoo.co.jp>
2023-05-03 23:55:40 +0900

    java1-3
    Change-Id: I91ef1cced074d7ac363f890eedd16632715f90d9

---
+++ HelloWorld_FamilynameFirstname/src/sample/SelfIntroduction.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class SelfIntroduction {
+	public static void main(String[] args) {
+	// 氏名
+	String name = "ネ一口田土也羊大糸エ";
+	// 年齢
+	int age = 11111111;
+	// 趣味
+	String hobby = "ない";
+	
+	/*
+	* 自己紹介文を出力する処理
+	*/
+	{
+	System.out.println("私の名前は" + name + "です。");
+	System.out.println("年齢は" + age + "歳です。");
+	System.out.println("趣味は" + hobby + "です。");
+
+		}
+	}
+}
