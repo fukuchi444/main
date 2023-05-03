6837638729ed3a2b30d180c2c4733f037c33a953
fukuchi_miku <108809977+fukuchi444@users.noreply.github.com>
2023-05-04 00:42:56 +0900

    Update selfIntroduction.java

--- HelloWorld/src/sample/selfIntroduction.java
+++ HelloWorld/src/sample/selfIntroduction.java
@@ -4,11 +4,11 @@ public class selfIntroduction {
 
 	public static void main(String[] args) { {
 		String name = "ネ一口田土也羊大糸エ";
-		int age = 10603;
+		int age = 111111;
 		String hobby = "ない";
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("私の名前は" + name + "です。");
-		System.out.println("生まれてから" + age + "日以上経過しました。");
+		System.out.println("年齢は" + age + "歳です。");
 		System.out.println("趣味は" + hobby + "です。");
 		}
 	}
