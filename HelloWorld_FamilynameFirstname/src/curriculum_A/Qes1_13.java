c27010da95a4335254547cb1ae9d4f38391a735b
Fukuchi_Miku <rikopin390@yahoo.co.jp>
2023-05-15 00:02:31 +0900

    問8まで完了、問9途中

--- HelloWorld_FamilynameFirstname/src/curriculum_A/Qes1_13.java
+++ HelloWorld_FamilynameFirstname/src/curriculum_A/Qes1_13.java
@@ -156,11 +156,73 @@ package curriculum_A;
 			
 			
 /* -----------------------------------------
- [概要] 問７　問６の自己紹介文に続き、BMIを出力する!!!!!!!!!!!!!
+ [概要] 問７　問６の自己紹介文に続き、BMIを出力する
  ------------------------------------------- */
 			//「BMIは○○です」
 			height = Math.pow(170.5, 2);
 			double bmi = ((double)Math.round(weight / (height / 10000) * 100))/100;
+			System.out.println(String.format("BMIは" + bmi + "です"));
+			
+			
+/* -------------------------------------------------
+ [概要] 問８　問６で宣言した変数に再代入し、下記の通り出力する
+ --------------------------------------------------- */
+			//初めまして鈴木一郎です
+			name = "鈴木一郎";
+			System.out.println("初めまして" + name + "です");
+			
+			//年齢は24歳です
+			age = 24;
+			System.out.println("年齢は" + age + "歳です");
+			
+			//身長168.5cmです
+			height = 168.5d;
+			System.out.println("身長は" + height + "cmです");
+			
+			//体重は64.2kgです
+			weight = 64.2d;
+			System.out.println("体重は" + weight + "kgです");
+			
+			//好きな食べ物はオムライスです
+			eat = "オムライス";
+			System.out.println("好きな食べ物は" + eat + "です");
+			
+			//BMIは22.6です
+			height = Math.pow(168.5, 2);
+			bmi = ((double)Math.round(weight / (height / 10000) * 10))/10;
+			System.out.println(String.format("BMIは" + bmi + "です"));
+			
+			
+/* ----------------------------------------
+ [概要] 問９　問８の変数【年齢・身長・体重】の数値を
+ 			和算で自己代入し、下記の通り出力する
+ ------------------------------------------ */
+			//初めまして鈴木一郎です
+			name = "鈴木一郎";
+			System.out.println("初めまして" + name + "です");
+			
+			//年齢は48歳です
+			int age2 = 24;
+			age = age + age2;
+			System.out.println("年齢は" + age + "歳です");
+			
+			//身長337.0cmです!!!!!!!!!!おかしい
+			double height2 = 168.5d;
+			height = height + height2;
+			System.out.println("身長は" + height + "cmです");
+			
+			//体重は128.4kgです
+			double weight2 = 64.2d;
+			weight = weight + weight2;
+			System.out.println("体重は" + weight + "kgです");
+			
+			//好きな食べ物はオムライスです
+			eat = "オムライス";
+			System.out.println("好きな食べ物は" + eat + "です");
+			
+			//BMIは11.31です
+			height = Math.pow(337.0, 2);
+			bmi = ((double)Math.round(weight / (height / 10000) * 10))/10;
 			System.out.println(String.format("BMIは" + bmi + "です"));
 		}
 	}
