eb81cdd2e7bba3c8a333823b8e553548ed795336
Fukuchi_Miku <rikopin390@yahoo.co.jp>
2023-06-15 10:34:48 +0900

    カリキュラムB_問4、問5

--- HelloWorld_FamilynameFirstname/src/curriculum_B/Qes4.java
+++ HelloWorld_FamilynameFirstname/src/curriculum_B/Qes4.java
@@ -1,5 +1,22 @@
 package curriculum_B;
 
-public class Qes4 {
 
+public class Qes4 {
+	public static void main(String[] args){
+		
+	/* -----------------------------------------------
+	 * [概要] 問４ for文を使用して九九のかけ算表を作成する
+	  　----------------------------------------------- */
+		System.out.println("問４");
+		
+		// 変数tateを、1～9まで繰り返す
+		for (int  tate = 1; tate < 10; tate++) {
+			// 変数yokoを、1～9まで繰り返す
+			for (int yoko = 1; yoko < 10; yoko++) {
+				// 数字が2桁に満たない場合は、2桁になるまで0を付けて九九の計算式を表示。一式ごとに || で区切る
+				System.out.print(String.format("%02d * %02d = %02d",tate,yoko,tate * yoko) + " || ");
+			}
+			System.out.print("\n");
+		}
+	}
 }
--- HelloWorld_FamilynameFirstname/src/curriculum_B/Qes6.java
+++ HelloWorld_FamilynameFirstname/src/curriculum_B/Qes6.java
@@ -1,5 +1,21 @@
 package curriculum_B;
 
 public class Qes6 {
-
+	public static void main(String[] args){
+		
+	/* -------------------------------------------
+	 * [概要] 問６ for文を使用して9×20までのかけ算をする
+	  　------------------------------------------- */
+		System.out.println("問６");
+		
+		// 変数tateを1～9まで繰り返す
+		for (int  tate = 1; tate < 10; tate++) {
+			// 変数yokoを1～20まで繰り返す
+			for (int yoko = 1; yoko < 21; yoko++) {
+				// 数字が3桁に満たない場合は、3桁になるまで0を付けて計算式を表示。一式ごとに || で区切る
+				System.out.print(String.format("%03d * %03d = %03d",tate,yoko,tate * yoko) + " || ");
+			}
+			System.out.print("\n");
+		}
+	}
 }
