c86f7d16e9c923f94712fb09be133f3dda0c849b
Fukuchi_Miku <rikopin390@yahoo.co.jp>
2023-07-27 12:31:15 +0900

    【再々提出_7/26】Q6

--- HelloWorld_FamilynameFirstname/src/curriculum_B/Qes6.java
+++ HelloWorld_FamilynameFirstname/src/curriculum_B/Qes6.java
@@ -49,19 +49,18 @@ public class Qes6 {
 			case "加湿器":
 				System.out.println("加湿器の残り台数は" + stock + "台です\n");
 				break;
+				
 
-			case "テレビ":
-				System.out.println("テレビの残り台数は" + stock + "台です\n");
-				break;
-
-			case "ディスプレイ":
-				//　テレビとディスプレイは同じ商品扱い
+			// 【7/26修正】 テレビのcase文を削除。テレビとディスプレイは同じ商品扱いなので、テレビとディスプレイのcase文を統合
+			case "テレビ","ディスプレイ":
+				
 				// 変数maxを用意し、最大個数（11個）を代入
-
-				// 【7/24　修正】 条件演算子を追加。equals()メソッドで文字列を判定。テレビの時はテレビを。ディスプレイの時は最大個数の11個から在庫数を引くよう修正
 				int max =11;
-				System.out.println(item.equals("テレビ") ?  "テレビ" : "ディスプレイの残り台数は" + (max - stock) + "台です\n");
+				
+				// 【7/26修正】 入力された商品名が、テレビの時は乱数を。ディスプレイの時は最大個数の11個から乱数を引いた数を表示させる
+				System.out.println(item.equals("テレビ") ?  "テレビの残り台数は" + stock + "台です\n" : "ディスプレイの残り台数は" + (max - stock) + "台です\n");
 				break;
+				
 
 			default :
 				System.out.println("『" + item + "』" + "は指定の商品ではありません。\n");
