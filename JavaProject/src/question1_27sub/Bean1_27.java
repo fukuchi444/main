65f5229b159d5c4804654eac24e5b8870b2d732e
Fukuchi_Miku <rikopin390@yahoo.co.jp>
2023-08-28 12:13:02 +0900

    【課題提出】1_27

---
+++ JavaProject/src/question1_27sub/Bean1_27.java
@@ -0,0 +1,119 @@
+package question1_27sub; // メソッド以外　処理用
+
+public class Bean1_27 {
+	/* ----------------------------------------------------------------------------------------
+	 * 【概要】 下記が入力されたときに、コンソール出力結果が表示されるように作成する
+ 			  ライオン:2.1:80,ゾウ:3.2:40,パンダ:1.9:30,チンパンジー:0.94:25,シマウマ:2.4:65,インコ:0.1:50
+
+  	コンソール出力結果
+
+  	コンソールに文字を入力してください
+  	動物名：ライオン
+	体長：2.1m
+	速度：80km/h
+	学名：パンテラ レオ
+
+	動物名：ゾウ
+	体長：3.2m
+	速度：40km/h
+	学名：ロキソドンタ・サイクロティス
+
+	動物名：パンダ
+	体長：1.9m
+	速度：30km/h
+	学名：アイルロポダ・メラノレウカ
+
+	動物名：チンパンジー
+	体長：0.94m
+	速度：25km/h
+	学名：パン・トゥログロディテス
+
+	動物名：シマウマ
+	体長：2.4m
+	速度：65km/h
+	学名：チャップマンシマウマ
+
+	動物名：インコ
+	体長：0.1m
+	速度：50km/h
+	学名：不明
+
+  --------------------------------------------------------------------------------------- */
+	// フィールドに変数を定義
+	private String lion;
+	private String elephant;
+	private String panda;
+	private String chimpanzee;
+	private String zebra;
+	private String parakeet;
+	
+
+	// コンストラクタを作成(引数あり)
+	public Bean1_27(String lion, String elephant, String panda, String chimpanzee, String zebra, String parakeet) {
+		this.lion = lion;
+		this.elephant = elephant;
+		this.panda = panda;
+		this.chimpanzee = chimpanzee;
+		this.zebra = zebra;
+		this.parakeet = parakeet;
+	}
+	
+	// コンストラクタを作成(引数なし)
+	public Bean1_27() {
+		this.lion = "パンテラ レオ";
+		this.elephant = "ロキソドンタ・サイクロティス";
+		this.panda = "アイルロポダ・メラノレウカ";
+		this.chimpanzee = "パン・トゥログロディテス";
+		this.zebra = "チャップマンシマウマ";
+		this.parakeet= "不明";
+	}
+	
+	
+	// 変数の値を取得する(getter)
+	public String getLion() {
+		return this.lion;
+	}
+
+	public String getElephant() {
+		return this.elephant;
+	}
+
+	public String getPanda() {
+		return this.panda;
+	}
+
+	public String getChimpanzee() {
+		return this.chimpanzee;
+	}
+	public String getZebra() {
+		return this.zebra;
+	}
+	public String getParakeet() {
+		return parakeet;
+	}
+	
+	
+	// 変数の値を設定する(setter)
+	public void setLion(String lion) {
+		this.lion = lion;
+	}
+	
+	public void setElephant(String elephant) {
+		this.elephant = elephant;
+	}
+	
+	public void setPanda(String panda) {
+		this.panda = panda;
+	}
+	
+	public void setChimpanzee(String chimpanzee) {
+		this.chimpanzee = chimpanzee;
+	}
+	public void setZebra(String zebra) {
+		this.zebra = zebra;
+	}
+	public void setParakeet(String parakeet) {
+		this.parakeet = parakeet;
+	}
+}
+
---
+++ JavaProject/src/question1_27sub/Sub1_27.java
@@ -0,0 +1,99 @@
+package question1_27sub; // メソッド　ぱしり 処理用
+
+import java.util.Scanner;
+
+public class Sub1_27 extends Bean1_27{
+
+
+	/* ----------------------------------------------------------------------------------------
+	 * 【概要】 下記が入力されたときに、コンソール出力結果が表示されるように作成する
+ 			  ライオン:2.1:80,ゾウ:3.2:40,パンダ:1.9:30,チンパンジー:0.94:25,シマウマ:2.4:65,インコ:0.1:50
+
+  	コンソール出力結果
+
+  	コンソールに文字を入力してください
+  	動物名：ライオン
+	体長：2.1m
+	速度：80km/h
+	学名：パンテラ レオ
+
+	動物名：ゾウ
+	体長：3.2m
+	速度：40km/h
+	学名：ロキソドンタ・サイクロティス
+
+	動物名：パンダ
+	体長：1.9m
+	速度：30km/h
+	学名：アイルロポダ・メラノレウカ
+
+	動物名：チンパンジー
+	体長：0.94m
+	速度：25km/h
+	学名：パン・トゥログロディテス
+
+	動物名：シマウマ
+	体長：2.4m
+	速度：65km/h
+	学名：チャップマンシマウマ
+
+	動物名：インコ
+	体長：0.1m
+	速度：50km/h
+	学名：不明
+
+  --------------------------------------------------------------------------------------- */
+
+	public void Name() {
+		// スキャナークラスを呼び出す
+		Scanner scan = new Scanner(System.in);
+
+		// 名前を入力してもらう文言を表示
+		System.out.println("コンソールに文字を入力してください\n");
+		String display = scan.next();
+
+		// 各項目名を入れた配列
+		String[] items = {"動物名","体長","速度"};
+
+
+		// 入力された値を配列に格納。指定の記号ごとに区切って単語にする
+		String[] animal = display.split("[,:]");
+
+
+		// 入力された値ぶん繰り返す。項目が3つあるため、iを3ずつ増やす
+		for (int i = 0; i < animal.length; i = i + 3){
+
+			System.out.println(items[0] + ":" + animal[i]);
+			System.out.println(items[1] + ":" + animal[i + 1]);
+			System.out.println(items[2] + ":" + animal[i + 2]);
+
+			// 動物名によって表示する学名を切り替える
+			switch (animal[i]) {
+			case "ライオン":
+				System.out.println("学名：" + super.getLion() + "\n");
+				break;
+
+			case "ゾウ":
+				System.out.println("学名：" + super.getElephant() + "\n");
+				break;
+
+			case "パンダ":
+				System.out.println("学名：" + super.getPanda() + "\n");
+				break;
+
+			case "チンパンジー":
+				System.out.println("学名：" + super.getChimpanzee() + "\n");
+				break;
+
+			case "シマウマ":
+				System.out.println("学名：" + super.getZebra() + "\n");
+				break;
+
+			case "インコ":
+				System.out.println("学名：" + super.getParakeet() + "\n");
+				break;
+			}
+		}
+		scan.close();
+	}
+}
